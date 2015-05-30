ac1005b51bd1d6dacbaabc6e16d88b53f30c8178
Ragzouken <ragzouken@gmail.com>
2015-05-30 12:10:32 +0100

    base code

---
+++ Assets/PlaneCamera/Camera.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PlaneCamera
+{
+    [SelectionBase]
+    public class Camera : MonoBehaviour
+    {
+        [Header("Reorientation Speed")]
+        public float focusTime = .25f;
+        public float pivotTime = .25f;
+        public float depthTime = .25f;
+
+        [Header("Orientation Limits")]
+        public float minPivot =  0;
+        public float maxPivot = 90;
+        public float minDepth =  5;
+        public float maxDepth = 15;
+
+        public Rect worldBounds;
+
+        [Header("Link Pivot & Zoom")]
+        public bool linkPivotToDepth;
+        public AnimationCurve pivotZoomCurve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+
+        [Header("Target Orientation")]
+        public Vector2 focusTarget;
+        public float pivotTarget;
+        public float depthTarget;
+
+        [Header("Internal Transforms")]
+        [SerializeField]
+        protected Transform depthTransform;
+        [SerializeField]
+        protected Transform pivotTransform;
+        [SerializeField]
+        protected Transform focusTransform;
+
+        public Vector2 focus
+        {
+            get
+            {
+                return new Vector2(focusTransform.position.x,
+                                   focusTransform.position.z);
+            }
+
+            protected set
+            {
+                focusTransform.position = new Vector3(value.x, 0, value.y);
+            }
+        }
+
+        public float pivot
+        {
+            get
+            {
+                return pivotTransform.localEulerAngles.x;
+            }
+
+            protected set
+            {
+                pivotTransform.localEulerAngles = Vector3.right * value;
+            }
+        }
+
+        public float depth
+        {
+            get
+            {
+                return -depthTransform.localPosition.z;
+            }
+
+            protected set
+            {
+                depthTransform.localPosition = Vector3.back * value;
+            }
+        }
+
+        private Vector2 focusVelocity;
+        private float pivotVelocity;
+        private float depthVelocity;
+
+        protected virtual void Awake()
+        {
+            focusTarget = focus;
+            pivotTarget = pivot;
+            depthTarget = depth;
+
+            ClampTargets();
+            SnapToTargets();
+        }
+
+        protected virtual void Update()
+        {
+            ClampTargets();
+
+            focus = Vector2.SmoothDamp(focus, focusTarget, ref focusVelocity, focusTime);
+            pivot = Mathf.SmoothDamp(pivot, pivotTarget, ref pivotVelocity, pivotTime);
+            depth = Mathf.SmoothDamp(depth, depthTarget, ref depthVelocity, depthTime);
+
+            if (linkPivotToDepth)
+            {
+                pivotTarget = PivotFromDepth(depth);
+                pivot = pivotTarget;
+            }
+        }
+
+        protected virtual void ClampTargets()
+        {
+            pivotTarget = Mathf.Clamp(pivotTarget, minPivot, maxPivot);
+            depthTarget = Mathf.Clamp(depthTarget, minDepth, maxDepth);
+            focusTarget = new Vector2(Mathf.Clamp(focusTarget.x, worldBounds.xMin, worldBounds.xMax),
+                                      Mathf.Clamp(focusTarget.y, worldBounds.yMin, worldBounds.yMax));
+        }
+
+        public virtual void SnapToTargets()
+        {
+            focus = focusTarget;
+            pivot = pivotTarget;
+            depth = depthTarget;
+        }
+
+        public virtual float PivotFromDepth(float depth)
+        {
+            float u = Mathf.InverseLerp(minDepth, maxDepth, depth);
+            float v = pivotZoomCurve.Evaluate(u);
+            float pivot = Mathf.Lerp(minPivot, maxPivot, v);
+
+            return pivot;
+        }
+    }
+}
---
+++ Assets/PlaneCamera/CameraInput.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PlaneCamera
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraInput : MonoBehaviour
+    {
+        protected new Camera camera;
+
+        public bool scrollZoom;
+        [Range(-8, 8)]
+        [Tooltip("Multiply the effect scrolling has on the camera depth target by 2^x")]
+        public float scrollDepthPower;
+
+
+
+        public bool keyboardPan;
+        public bool scalePan;
+
+        [Range(-8, 8)]
+        public float panPower;
+
+        protected virtual void Awake()
+        {
+            camera = GetComponent<Camera>();
+        }
+
+        protected virtual void Update()
+        {
+            if (scrollZoom)
+            {
+                camera.depthTarget -= Input.GetAxis("Mouse ScrollWheel") * Mathf.Pow(2, scrollDepthPower);
+            }
+
+            Vector2 pan = Vector2.zero;
+
+            if (keyboardPan)
+            {
+                pan += Vector2.up    * Input.GetAxis("Vertical")   * Mathf.Pow(2, panPower)
+                    +  Vector2.right * Input.GetAxis("Horizontal") * Mathf.Pow(2, panPower);
+            }
+
+            if (scalePan)
+            {
+                pan *= camera.depth / camera.maxDepth;
+            }
+
+            camera.focusTarget += pan;
+        }
+    }
+}
